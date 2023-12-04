e5cf609b45cfc607e564aa5be2e1e84c155e0768
Dev Shah <97539345+busycaesar@users.noreply.github.com>
2023-12-04 10:28:32 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
-/*********************************************************************************
- *  WEB322 – Assignment 06
- *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part
- *  of this assignment has been copied manually or electronically from any other source
- *  (including 3rd party web sites) or distributed to other students.
- *
- *  Name: DEV JIGISHKUMAR SHAH Student ID: 131623217 Date: 02/12/2022
- *
- *  Online (Heroku) Link: https://web322app.onrender.com
- *
- ********************************************************************************/
-
 // ==> INCLUDING MODULES
 
 const express = require("express");
